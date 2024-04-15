cc6dd09359e00c3560f7bda4645f265c6ddd99e8
Devanshu046 <dev44908@gmail.com>
2024-04-15 11:06:12 +0530

    commit

---
+++ callback-hook/src/App.js
@@ -0,0 +1,13 @@
+import logo from './logo.svg';
+import './App.css';
+import ParentComponent from './component/ParentComponent';
+
+function App() {
+  return (
+    <div className="App">
+      <ParentComponent/>
+    </div>
+  );
+}
+
+export default App;
